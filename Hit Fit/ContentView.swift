1463fad7b2a85f222b62187361baf5d25027c18c
Mustafa-Nour <109715325+Mustafa-Nour@users.noreply.github.com>
2026-03-02 10:59:53 +0200

    Initial Commit

---
+++ Hit Fit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hit Fit
+//
+//  Created by Mustafa Nour on 02/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hit Fit/Hit_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hit_FitApp.swift
+//  Hit Fit
+//
+//  Created by Mustafa Nour on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Hit_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
